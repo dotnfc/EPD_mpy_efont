5889ddaa87b560ffe7319cff527ee2366f9fc072
root <root@DESKTOP-EEO63ML>
2023-09-09 17:59:47 +0800

    add unix port vscode remote debug in launch.json. add hello.py

---
+++ examples/hello.py
@@ -0,0 +1,9 @@
+#-*- coding:utf-8 -*-
+# 
+def main():
+    print("================================")
+    print("Hello World!")
+    print("================================")
+    
+if __name__ == "__main__":
+    main()
